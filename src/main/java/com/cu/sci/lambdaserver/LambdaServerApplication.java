15370d02688e89279706a50df1af37aafc645457
Mahmoud Atef(Bakar) <MahmoudAtef.coder@gmail.com>
2024-02-27 22:05:21 +0200

    first commit

---
+++ src/main/java/com/cu/sci/lambdaserver/LambdaServerApplication.java
@@ -0,0 +1,13 @@
+package com.cu.sci.lambdaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LambdaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LambdaServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cu/sci/lambdaserver/LambdaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cu.sci.lambdaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LambdaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
