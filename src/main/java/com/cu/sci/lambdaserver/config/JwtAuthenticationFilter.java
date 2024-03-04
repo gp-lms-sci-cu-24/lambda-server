02c4a9239ad66cd7a8e49a2e73afbcf18bcef885
omar <202027471@std.sci.cu.edu.eg>
2024-03-04 02:41:55 +0200

    update JwtAuthenticationFilter

--- src/main/java/com/cu/sci/lambdaserver/config/JwtAuthenticationFilter.java
+++ src/main/java/com/cu/sci/lambdaserver/config/JwtAuthenticationFilter.java
@@ -6,8 +6,11 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.lang.NonNull;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 import java.io.IOException;
@@ -16,6 +19,7 @@ import java.io.IOException;
 @RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
     private final JwtService jwtService ;
+    private final UserDetailsService userDetailsService;
 
     @Override
     protected void doFilterInternal(
@@ -28,20 +32,36 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         final String authHeader = request.getHeader("Authorization");
 
         final String jwt;
-        final  String userId;
-        if(authHeader==null||!authHeader.startsWith("Bearer ")){
-            filterChain.doFilter(request,response);
+        final String userId;
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            filterChain.doFilter(request, response);
             return;
         }
         jwt = authHeader.substring(7);
         //TODO here we need to extract the user id from JWT token
+
         userId = jwtService.extractUserId(jwt);
+        if (userId != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+          UserDetails userDetails = this.userDetailsService.loadUserByUsername(userId);
+            //TODO here we need to cheek if the token is valid or not
 
-        //TODO @ HNOONa-0 - Continue implementation here
+            if (jwtService.isTokenValid(jwt,userDetails)){
+                //TODO here we need to update the securityContextHolder
 
-//        if(userId!=null && SecurityContextHolder.getContext().getAuthentication()==null){
-//            UserDetails userDetails;
-//
-//        }
+                UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
+                        userDetails,
+                        null,
+                        userDetails.getAuthorities()
+                );
+                authToken.setDetails(
+                        new WebAuthenticationDetailsSource().buildDetails(request)
+                );
+                //update finally here
+                SecurityContextHolder.getContext().setAuthentication(authToken);
+
+            }
+        }
+        filterChain.doFilter(request,response);
+
     }
 }
