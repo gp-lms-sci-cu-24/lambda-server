95f47e568478529a596ad627c2cf03f9c3e457f7
omar <202027471@std.sci.cu.edu.eg>
2024-03-04 13:03:22 +0200

    update user repo and hide user service & controller

--- src/main/java/com/cu/sci/lambdaserver/UserPackage/User.java
+++ src/main/java/com/cu/sci/lambdaserver/UserPackage/User.java
@@ -5,16 +5,21 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.experimental.SuperBuilder;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
 @Entity
-@Table(name="users")
+@Table(name = "users")
 @PrimaryKeyJoinColumn(name = "user_id")
-public class User {
+public class User implements UserDetails {
     @Id
     private String userId;
     private String password;
@@ -25,7 +30,42 @@ public class User {
     private String nationalId;
     private String photoUrl;
     private int age;
-
     @Enumerated(EnumType.STRING)
     private Role role = Role.USER;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+
+        return true;
+
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+
+        return true;
+    }
 }
+
--- src/main/java/com/cu/sci/lambdaserver/UserPackage/UserRepository.java
+++ src/main/java/com/cu/sci/lambdaserver/UserPackage/UserRepository.java
@@ -1,8 +1,11 @@
 package com.cu.sci.lambdaserver.UserPackage;
 
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import java.util.Optional;
 
-@Repository
-public interface UserRepository extends CrudRepository<User,String> {
+@RepositoryRestResource
+public interface UserRepository extends JpaRepository<User,Integer>{
+    Optional<User> findByuserId(String id);
+
 }
