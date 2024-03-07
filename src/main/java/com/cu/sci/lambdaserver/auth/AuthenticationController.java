c4071e72dd1b02b6e53be1821d305de8885d4c3f
omar <202027471@std.sci.cu.edu.eg>
2024-03-07 03:46:50 +0200

    update AuthenticationController

--- src/main/java/com/cu/sci/lambdaserver/auth/AuthenticationController.java
+++ src/main/java/com/cu/sci/lambdaserver/auth/AuthenticationController.java
@@ -1,26 +1,48 @@
 package com.cu.sci.lambdaserver.auth;
 
+import com.cu.sci.lambdaserver.UserPackage.User;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 @RequestMapping("/api/v1/auth")
 @RequiredArgsConstructor
 public class AuthenticationController {
+
     private final AuthenticationService service;
+    private final AuthenticationManager authenticationManager;
+
     @PostMapping("/register")
-    public ResponseEntity<AuthenticationResponse> register(
+    public ResponseEntity<User> register(
             @RequestBody RegisterRequest request
-    ){
+    ) {
         return ResponseEntity.ok(service.register(request));
     }
+//  @PostMapping("/authenticate")
+//  public ResponseEntity<AuthenticationResponse> authenticate(
+//      @RequestBody AuthenticationRequest request
+//  ) {
+//    return ResponseEntity.ok(service.authenticate(request));
+//  }
 
-
-    @PostMapping("/authenticate")
-    public ResponseEntity<AuthenticationResponse> authenticate(
+    @PostMapping("/signin")
+    public ResponseEntity<String> signin(
             @RequestBody AuthenticationRequest request
-    ){
-        return ResponseEntity.ok(service.authenticate(request));
+    ) {
+        Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(
+                request.getId(),request.getPassword()
+        ));
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        return new ResponseEntity<>(authentication.getName(), HttpStatus.OK);
     }
 }
