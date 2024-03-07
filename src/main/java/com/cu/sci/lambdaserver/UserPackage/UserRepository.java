18228db7e75e50b338e66d1451cee3b599a1efe9
omar <202027471@std.sci.cu.edu.eg>
2024-03-07 03:46:27 +0200

    update UserRepository

--- src/main/java/com/cu/sci/lambdaserver/UserPackage/UserRepository.java
+++ src/main/java/com/cu/sci/lambdaserver/UserPackage/UserRepository.java
@@ -1,11 +1,14 @@
 package com.cu.sci.lambdaserver.UserPackage;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-@RepositoryRestResource
-public interface UserRepository extends JpaRepository<User,Integer>{
-    Optional<User> findByuserId(String id);
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+
+    //  Optional<User> findByEmail(String email);
+    Optional<User> findById(String id);
 
 }
