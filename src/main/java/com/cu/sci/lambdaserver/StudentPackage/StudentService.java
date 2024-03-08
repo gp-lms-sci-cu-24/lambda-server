6add40522dbd7a6d47cd0014f32c46bf3e7d7ae4
Abdallah Mohamed <abdallah.moh.153@gmail.com>
2024-03-08 22:44:57 +0200

    mplementation of StudentService

--- src/main/java/com/cu/sci/lambdaserver/StudentPackage/StudentService.java
+++ src/main/java/com/cu/sci/lambdaserver/StudentPackage/StudentService.java
@@ -3,8 +3,37 @@ package com.cu.sci.lambdaserver.StudentPackage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
 public class StudentService {
     @Autowired
     private StudentRepository studentRepository ;
+
+    public List<Student> getAllStudents(){
+        return studentRepository.findAll() ;
+    }
+
+    public Optional<Student> getStudent(Long id){
+        return studentRepository.findById(id) ;
+    }
+
+    public Student createStudent(Student student){
+        return studentRepository.save(student) ;
+    }
+
+    public Student updateStudent(Student studentDetails , Long id){
+        Student student = studentRepository.findById(id).orElseThrow(()->new RuntimeException("Student Not Found With This Id" + id)) ;
+        student.setLevel(studentDetails.getLevel());
+        student.setCreditHours(studentDetails.getCreditHours());
+        student.setDepartment(studentDetails.getDepartment());
+        return studentRepository.save(student) ;
+    }
+
+    public void deleteStudent (Student studentDetails , Long id){
+        Student student = studentRepository.findById(id).orElseThrow(()->new RuntimeException("Student Not Found With This Id" + id)) ;
+        studentRepository.delete(student);
+
+    }
 }
