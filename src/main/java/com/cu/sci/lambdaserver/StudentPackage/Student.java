2f729880a8b9c2738190e267e9b5f49c8c5bd07c
Abdallah Mohamed <abdallah.moh.153@gmail.com>
2024-03-08 18:44:57 +0200

    add student service

--- src/main/java/com/cu/sci/lambdaserver/StudentPackage/Student.java
+++ src/main/java/com/cu/sci/lambdaserver/StudentPackage/Student.java
@@ -16,6 +16,62 @@ public class Student {
     private Integer level ;
     private String department ;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getCode() {
+        return code;
+    }
+
+    public void setCode(Long code) {
+        this.code = code;
+    }
+
+    public Integer getCreditHours() {
+        return creditHours;
+    }
+
+    public void setCreditHours(Integer creditHours) {
+        this.creditHours = creditHours;
+    }
+
+    public Integer getLevel() {
+        return level;
+    }
+
+    public void setLevel(Integer level) {
+        this.level = level;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public Set<Course> getCourses() {
+        return courses;
+    }
+
+    public void setCourses(Set<Course> courses) {
+        this.courses = courses;
+    }
+
     @ManyToMany
     @JoinTable(
             name = "student_courses",
---
+++ src/main/java/com/cu/sci/lambdaserver/StudentPackage/StudentService.java
@@ -0,0 +1,10 @@
+package com.cu.sci.lambdaserver.StudentPackage;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class StudentService {
+    @Autowired
+    private StudentRepository studentRepository ;
+}
