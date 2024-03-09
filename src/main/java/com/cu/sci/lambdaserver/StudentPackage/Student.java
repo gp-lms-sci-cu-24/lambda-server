5162855afedf0b6962061090e79482d09743a1ff
Abdallah Mohamed <abdallah.moh.153@gmail.com>
2024-03-10 01:52:56 +0200

    modifiy Student enity and add validation on its proprites

--- src/main/java/com/cu/sci/lambdaserver/StudentPackage/Student.java
+++ src/main/java/com/cu/sci/lambdaserver/StudentPackage/Student.java
@@ -1,77 +1,46 @@
 package com.cu.sci.lambdaserver.StudentPackage;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 
 import java.util.HashSet;
 import java.util.Set;
 
 @Entity
+@Table
 public class Student {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String name ;
+
+    @NotNull()
+    @NotEmpty()
+    private String firstName ;
+    @NotNull()
+    @NotEmpty()
+    private String lastName ;
+    @NotNull()
+    @NotEmpty()
+    @Column(unique = true)
     private Long code ;
-    private Integer creditHours ;
+    private Integer creditHours = 0 ;
+    @NotNull()
+    @NotEmpty()
+    private String address ;
+    @NotNull()
+    @NotEmpty()
+    private Integer phoneNumber ;
+    @NotNull()
+    @NotEmpty()
+    private  String highSchoolmajor ;
+    private Double gpa  = 0.0 ;
+    @NotNull()
+    @NotEmpty()
     private Integer level ;
+    @NotNull()
+    @NotEmpty()
     private String department ;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Long getCode() {
-        return code;
-    }
-
-    public void setCode(Long code) {
-        this.code = code;
-    }
-
-    public Integer getCreditHours() {
-        return creditHours;
-    }
-
-    public void setCreditHours(Integer creditHours) {
-        this.creditHours = creditHours;
-    }
-
-    public Integer getLevel() {
-        return level;
-    }
-
-    public void setLevel(Integer level) {
-        this.level = level;
-    }
-
-    public String getDepartment() {
-        return department;
-    }
-
-    public void setDepartment(String department) {
-        this.department = department;
-    }
-
-    public Set<Course> getCourses() {
-        return courses;
-    }
-
-    public void setCourses(Set<Course> courses) {
-        this.courses = courses;
-    }
-
     @ManyToMany
     @JoinTable(
             name = "student_courses",
@@ -81,5 +50,34 @@ public class Student {
     private Set<Course> courses = new HashSet<>() ;
 
 
+    public Student() {
+    }
 
+    public Student(String firstName, String lastName, Long code, Integer creditHours, String address, Integer phoneNumber, String highSchoolmajor, Double gpa, Integer level, String department, Set<Course> courses) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.code = code;
+        this.creditHours = creditHours;
+        this.address = address;
+        this.phoneNumber = phoneNumber;
+        this.highSchoolmajor = highSchoolmajor;
+        this.gpa = gpa;
+        this.level = level;
+        this.department = department;
+        this.courses = courses;
+    }
+
+
+    public Student(String firstName, String lastName, Long code, Integer creditHours, String address, Integer phoneNumber, String highSchoolmajor, Double gpa, Integer level, String department) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.code = code;
+        this.creditHours = creditHours;
+        this.address = address;
+        this.phoneNumber = phoneNumber;
+        this.highSchoolmajor = highSchoolmajor;
+        this.gpa = gpa;
+        this.level = level;
+        this.department = department;
+    }
 }
