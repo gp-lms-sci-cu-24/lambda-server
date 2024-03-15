5a0b05fec88e8dd3fd9c002f4502e904b5d49ea1
Abdallah Mohamed <abdallah.moh.153@gmail.com>
2024-03-15 23:25:12 +0200

    add findByCode

--- src/main/java/com/cu/sci/lambdaserver/student/StudentRepository.java
+++ src/main/java/com/cu/sci/lambdaserver/student/StudentRepository.java
@@ -3,6 +3,9 @@ package com.cu.sci.lambdaserver.student;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface StudentRepository extends JpaRepository<Student,Long> {
+    Optional<Student> findByCode(String code);
 }
