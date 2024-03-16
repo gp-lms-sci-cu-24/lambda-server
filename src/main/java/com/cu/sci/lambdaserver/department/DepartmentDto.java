624818a196da6cd385d9a96110aa9c8d0275440f
Abdallah Mohamed <abdallah.moh.153@gmail.com>
2024-03-16 22:58:35 +0200

    add DepartmentDto

---
+++ src/main/java/com/cu/sci/lambdaserver/department/DepartmentDto.java
@@ -0,0 +1,21 @@
+package com.cu.sci.lambdaserver.department;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class DepartmentDto {
+
+    private Long id;
+
+    private String name;
+
+    private String info;
+
+    private String image;
+}
