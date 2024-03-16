1be68f42fe5c0f3f2a6e0c30789cce42753e997e
Mohamed atef <89284188+hngara@users.noreply.github.com>
2024-03-16 23:55:31 +0200

    service interface

---
+++ src/main/java/com/cu/sci/lambdaserver/course/ICourseService.java
@@ -0,0 +1,19 @@
+package com.cu.sci.lambdaserver.course;
+
+import java.util.List;
+
+public interface ICourseService {
+    List<Course> GetCourses();
+
+    Course getCourse(Long courseId);
+
+    void addCourse(Course course);
+
+    void deleteCourse(Long courseId);
+
+    void updateCourse(Long courseId,
+                      String name,
+                      String code,
+                      String info,
+                      Integer creditHours);
+}
