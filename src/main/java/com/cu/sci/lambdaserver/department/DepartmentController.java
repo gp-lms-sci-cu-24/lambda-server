3635be1e716d1d4dbb24de2ea21fd6207da129fe
Abdallah Mohamed <abdallah.moh.153@gmail.com>
2024-03-17 01:01:41 +0200

    implemention postcontroller

--- src/main/java/com/cu/sci/lambdaserver/department/DepartmentController.java
+++ src/main/java/com/cu/sci/lambdaserver/department/DepartmentController.java
@@ -4,6 +4,9 @@ package com.cu.sci.lambdaserver.department;
 import com.cu.sci.lambdaserver.department.services.DepartmentService;
 import com.cu.sci.lambdaserver.utils.mapper.config.iMapper;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,4 +17,11 @@ public class DepartmentController {
 
     private final DepartmentService departmentService;
     private final iMapper<Department, DepartmentDto> departmentMapper;
+
+    @PostMapping
+    public ResponseEntity createDepartment(DepartmentDto departmentDto){
+        Department department = departmentMapper.mapFrom(departmentDto) ;
+        departmentService.createDepartment(department) ;
+        return new ResponseEntity(HttpStatus.CREATED) ;
+    }
 }
