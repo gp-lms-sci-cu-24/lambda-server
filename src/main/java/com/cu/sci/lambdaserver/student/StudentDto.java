4942e80df486abca1b02d00519a9854aa40d9741
Abdallah Mohamed <abdallah.moh.153@gmail.com>
2024-03-19 22:13:52 +0200

    solve student's issue

--- src/main/java/com/cu/sci/lambdaserver/student/StudentDto.java
+++ src/main/java/com/cu/sci/lambdaserver/student/StudentDto.java
@@ -13,6 +13,10 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class StudentDto {
 
+    private String userName;
+
+    private String password;
+
     private String firstName;
 
     private String fatherName;
