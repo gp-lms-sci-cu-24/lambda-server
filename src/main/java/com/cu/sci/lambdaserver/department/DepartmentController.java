f3f3530626884299b35b8b072a72b6defc48587c
Abdallah Mohamed <abdallah.moh.153@gmail.com>
2024-03-20 16:23:50 +0200

    refactor pagnation for department

--- src/main/java/com/cu/sci/lambdaserver/department/DepartmentController.java
+++ src/main/java/com/cu/sci/lambdaserver/department/DepartmentController.java
@@ -2,7 +2,6 @@ package com.cu.sci.lambdaserver.department;
 
 
 import com.cu.sci.lambdaserver.department.services.DepartmentService;
-import com.cu.sci.lambdaserver.student.Student;
 import com.cu.sci.lambdaserver.student.StudentDto;
 import com.cu.sci.lambdaserver.student.mapper.StudentMapper;
 import com.cu.sci.lambdaserver.utils.mapper.config.iMapper;
@@ -23,39 +22,39 @@ public class DepartmentController {
 
     private final DepartmentService departmentService;
     private final iMapper<Department, DepartmentDto> departmentMapper;
-    private final StudentMapper studentMapper ;
+    private final StudentMapper studentMapper;
 
     @PostMapping
-    public ResponseEntity createDepartment(@RequestBody DepartmentDto departmentDto){
-        Department department = departmentMapper.mapFrom(departmentDto) ;
-        departmentService.createDepartment(department) ;
-        return new ResponseEntity(HttpStatus.CREATED) ;
+    public ResponseEntity createDepartment(@RequestBody DepartmentDto departmentDto) {
+        Department department = departmentMapper.mapFrom(departmentDto);
+        departmentService.createDepartment(department);
+        return new ResponseEntity(HttpStatus.CREATED);
     }
 
     @GetMapping
-    public ResponseEntity<Page<DepartmentDto>> getDepartments(@RequestParam Integer pageNo , @RequestParam Integer pageSize){
-        Page<Department> departmentPage = departmentService.getAllDepartments(pageNo,pageSize) ;
-        Page<DepartmentDto> dtoPage =departmentPage.map(departmentMapper::mapTo) ;
-        return new ResponseEntity<>(dtoPage,HttpStatus.OK) ;
+    public ResponseEntity<Page<DepartmentDto>> getDepartments(@RequestParam Integer pageNo, @RequestParam Integer pageSize) {
+        Page<Department> departmentPage = departmentService.getAllDepartments(pageNo, pageSize);
+        Page<DepartmentDto> dtoPage = departmentPage.map(departmentMapper::mapTo);
+        return new ResponseEntity<>(dtoPage, HttpStatus.OK);
     }
 
     @GetMapping(path = "/{id}")
-    public ResponseEntity getDepartment(@PathVariable("id") long id){
-        try{
-            Department department = departmentService.getDepartmentByid(id) ;
-            return new ResponseEntity<>(departmentMapper.mapTo(department),HttpStatus.FOUND) ;
-        }catch (EntityNotFoundException e){
-            return new ResponseEntity<>(e.getMessage(),HttpStatus.NOT_FOUND) ;
+    public ResponseEntity getDepartment(@PathVariable("id") long id) {
+        try {
+            Department department = departmentService.getDepartmentByid(id);
+            return new ResponseEntity<>(departmentMapper.mapTo(department), HttpStatus.FOUND);
+        } catch (EntityNotFoundException e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
         }
     }
 
     @GetMapping(path = "/{id}/students")
-    public ResponseEntity getStudentsofDepartment(@PathVariable("id")Long id){
+    public ResponseEntity getStudentsofDepartment(@PathVariable("id") Long id) {
         try {
-            List <StudentDto> students = departmentService.getStudentsdepartment(id).stream().map(studentMapper::mapTo).collect(Collectors.toList());
-            return new ResponseEntity(students,HttpStatus.OK) ;
-        }catch (EntityNotFoundException e){
-            return new ResponseEntity(e.getMessage(),HttpStatus.NOT_FOUND) ;
+            List<StudentDto> students = departmentService.getStudentsdepartment(id).stream().map(studentMapper::mapTo).collect(Collectors.toList());
+            return new ResponseEntity(students, HttpStatus.OK);
+        } catch (EntityNotFoundException e) {
+            return new ResponseEntity(e.getMessage(), HttpStatus.NOT_FOUND);
         }
     }
 
--- src/main/java/com/cu/sci/lambdaserver/department/services/DepartmentService.java
+++ src/main/java/com/cu/sci/lambdaserver/department/services/DepartmentService.java
@@ -21,43 +21,43 @@ public class DepartmentService implements iDepartmentService {
 
     @Override
     public Department createDepartment(Department department) {
-        return departmentRepository.save(department) ;
+        return departmentRepository.save(department);
     }
 
     @Override
-    public Page<Department> getAllDepartments(Integer pageNo,Integer pageSize) {
-        Pageable pageable = PageRequest.of(pageNo,pageSize) ;
-        return departmentRepository.findAll(pageable) ;
+    public Page<Department> getAllDepartments(Integer pageNo, Integer pageSize) {
+        Pageable pageable = PageRequest.of(pageNo, pageSize);
+        return departmentRepository.findAll(pageable);
     }
 
     @Override
     public Department getDepartmentByid(Long id) {
         return departmentRepository.findById(id)
-                .orElseThrow(()->new EntityNotFoundException("Department with ID " + id + " does not exist")) ;
+                .orElseThrow(() -> new EntityNotFoundException("Department with ID " + id + " does not exist"));
     }
 
     @Override
     public Department getDepartmentByname(String name) {
         return departmentRepository.findDepartmentByName(name)
-                .orElseThrow(()->new EntityNotFoundException("Department with Name " + name + " does not exist")) ;
+                .orElseThrow(() -> new EntityNotFoundException("Department with Name " + name + " does not exist"));
     }
 
     @Override
-    public Department updateDepartment(Long id,Department department) {
-        if(!departmentRepository.existsById(id)){
-            throw new EntityNotFoundException("Department with ID " + id + " does not exist") ;
+    public Department updateDepartment(Long id, Department department) {
+        if (!departmentRepository.existsById(id)) {
+            throw new EntityNotFoundException("Department with ID " + id + " does not exist");
         }
-        return departmentRepository.findById(id).map(exsistingDepartment ->{
+        return departmentRepository.findById(id).map(exsistingDepartment -> {
             Optional.ofNullable(department.getName()).ifPresent(exsistingDepartment::setName);
             Optional.ofNullable(department.getInfo()).ifPresent(exsistingDepartment::setInfo);
-            return departmentRepository.save(exsistingDepartment) ;
-        }).orElseThrow(()->new EntityNotFoundException("Department with ID " + id + " does not exist")) ;
+            return departmentRepository.save(exsistingDepartment);
+        }).orElseThrow(() -> new EntityNotFoundException("Department with ID " + id + " does not exist"));
     }
 
     @Override
     public List<Student> getStudentsdepartment(Long id) {
         return departmentRepository.findById(id)
-                .orElseThrow(()->new EntityNotFoundException("Department with Id " + id + " does not exist"))
+                .orElseThrow(() -> new EntityNotFoundException("Department with Id " + id + " does not exist"))
                 .getStudents()
                 .stream()
                 .toList();
