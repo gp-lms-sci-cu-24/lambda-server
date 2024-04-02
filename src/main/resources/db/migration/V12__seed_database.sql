c99a515d0a3e53f820ff80e4f85c7c8d0325c755
Mahmoud Atef <Mahmoudbakar2002@github.com>
2024-04-02 05:09:50 +0200

    add seed data

---
+++ src/main/resources/db/migration/V12__seed_database.sql
@@ -0,0 +1,9 @@
+INSERT INTO public.users(id, username, password, profile_picture, is_account_non_expired, is_account_non_locked,
+                         is_credentials_non_expired, is_enabled)
+VALUES (1, 'admin', '$2y$10$29mW8AIZP73kHXC/u2FKw.p7oQcErTJMMWPlusKlnGUfdjWWrGexG', null, true, true, true, true);
+
+INSERT INTO public.user_roles(user_id, roles)
+VALUES (1, 'SUPER_ADMIN');
+
+INSERT INTO public.departments(id, name, info, image, code)
+VALUES (1, 'General', '', null, 'GEN');
