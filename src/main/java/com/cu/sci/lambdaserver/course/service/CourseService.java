c184943620b28928732fbed744ec96d91b8a66fc
Mohamed atef <89284188+hngara@users.noreply.github.com>
2024-04-04 01:09:04 +0200

    exception handling and rewrite passed value through the requests

--- src/main/java/com/cu/sci/lambdaserver/course/service/CourseService.java
+++ src/main/java/com/cu/sci/lambdaserver/course/service/CourseService.java
@@ -2,10 +2,11 @@ package com.cu.sci.lambdaserver.course.service;
 
 import com.cu.sci.lambdaserver.course.CourseRepository;
 import com.cu.sci.lambdaserver.course.entites.Course;
-import jakarta.persistence.EntityNotFoundException;
-import jakarta.persistence.NonUniqueResultException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 import java.util.Objects;
@@ -13,6 +14,7 @@ import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class CourseService implements ICourseService {
     private final CourseRepository courseRepository;
 
@@ -20,18 +22,18 @@ public class CourseService implements ICourseService {
         return courseRepository.findAll();
     }
 
+
     public Course getCourse(Long courseId) {
         Optional<Course> optionalCourse = courseRepository.findById(courseId);
         if (optionalCourse.isPresent())
             return optionalCourse.get();
-        throw new EntityNotFoundException("the course with id : " + courseId + " doesn't exist");
+        throw new ResponseStatusException(HttpStatus.NOT_FOUND, "the course with id : " + courseId + " doesn't exist");
     }
-
 
     public Course createCourse(Course course) {
         Optional<Course> courseWithSameCode = courseRepository.findByCode(course.getCode());
         if (courseWithSameCode.isPresent())
-            throw new NonUniqueResultException("this code already exist " + course.getCode());
+            throw new ResponseStatusException(HttpStatus.CONFLICT, "this code already exist " + course.getCode());
         return courseRepository.save(course);
     }
 
@@ -41,7 +43,7 @@ public class CourseService implements ICourseService {
             courseRepository.deleteById(courseId);
             return optionalCourse.get();
         }
-        throw new EntityNotFoundException("the course with id : " + courseId + " doesn't exist ");
+        throw new ResponseStatusException(HttpStatus.NOT_FOUND, "the course with id : " + courseId + " doesn't exist ");
     }
 
     @Override
@@ -53,10 +55,10 @@ public class CourseService implements ICourseService {
             Optional.ofNullable(course.getInfo()).ifPresent(currentCourse::setInfo);
             Optional<Course> courseWithSameCode = courseRepository.findByCode(course.getCode());
             if (courseWithSameCode.isPresent() && !Objects.equals(courseWithSameCode.get().getId(), courseId))
-                throw new NonUniqueResultException("this code already exist " + course.getCode());
+                throw new ResponseStatusException(HttpStatus.CONFLICT, "this code already exist " + course.getCode());
             else
                 Optional.ofNullable(course.getCode()).ifPresent(currentCourse::setCode);
             return courseRepository.save(currentCourse);
-        }).orElseThrow(() -> new EntityNotFoundException("course with ID " + courseId + " does not exist"));
+        }).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "the course with id : " + courseId + " doesn't exist "));
     }
 }
--- src/main/java/com/cu/sci/lambdaserver/coursePrerequisties/CoursePrerequisitesController.java
+++ src/main/java/com/cu/sci/lambdaserver/coursePrerequisties/CoursePrerequisitesController.java
@@ -1,22 +1,25 @@
 package com.cu.sci.lambdaserver.coursePrerequisties;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping(path = ("/api/v1/prerequisite"))
+@RequestMapping(path = ("/api/v1/course"))
 @RequiredArgsConstructor
 public class CoursePrerequisitesController {
     private final CoursePrerequisitesService coursePrerequisitesService;
 
-    @PostMapping
-    public void createPrerequisite(@RequestBody CoursePrerequisites coursePrerequisites) {
-        coursePrerequisitesService.createPrerequisite(coursePrerequisites);
+    @PostMapping("/{id}/prerequisites/{prerequisite}")
+    @ResponseStatus(HttpStatus.CREATED)
+    public CoursePrerequisites addPrerequisite(@PathVariable(name = "id") Long courseId, @PathVariable(name = "prerequisite") Long prerequisiteId) {
+        return coursePrerequisitesService.addPrerequisite(courseId, prerequisiteId);
     }
 
-    @GetMapping("/{id}")
+    @GetMapping("/{id}/prerequisites")
+    @ResponseStatus(HttpStatus.OK)
     public List<Long> getPrerequisite(@PathVariable(name = "id") Long id) {
         return coursePrerequisitesService.getPrerequisite(id);
     }
