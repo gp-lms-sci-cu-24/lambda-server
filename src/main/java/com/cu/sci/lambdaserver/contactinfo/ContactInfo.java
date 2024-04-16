596a210be45ae18e0dc6f9830c655e7416ba2f4d
Abdallah Mohamed <abdallah.moh.153@gmail.com>
2024-04-16 19:43:16 +0200

    add contact repo and entity

---
+++ src/main/java/com/cu/sci/lambdaserver/contactinfo/ContactInfo.java
@@ -0,0 +1,32 @@
+package com.cu.sci.lambdaserver.contactinfo;
+
+
+import com.cu.sci.lambdaserver.user.User;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "contact_info")
+public class ContactInfo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "contact_info_seq")
+    @SequenceGenerator(name = "contact_info_seq", sequenceName = "contact_info_seq", allocationSize = 10)
+    private Long id ;
+
+    @OneToOne()
+    @JoinColumn(name = "user_id")
+    private User user ;
+
+    private String phone ;
+
+    private String telephone ;
+
+    private String email ;
+
+}
---
+++ src/main/java/com/cu/sci/lambdaserver/contactinfo/ContactInfoRepository.java
@@ -0,0 +1,10 @@
+package com.cu.sci.lambdaserver.contactinfo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface ContactInfoRepository extends JpaRepository<ContactInfo,Long> {
+
+    Optional<ContactInfo> findByUser_Id(Long userId) ;
+}
