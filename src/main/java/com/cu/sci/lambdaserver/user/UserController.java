1bbebfcc58531fab868352671269e1cff6a88e2a
Mahmoud Atef <Mahmoudbakar2002@github.com>
2024-04-19 21:08:09 +0200

    user my state.

--- src/main/java/com/cu/sci/lambdaserver/user/UserController.java
+++ src/main/java/com/cu/sci/lambdaserver/user/UserController.java
@@ -1,6 +1,8 @@
 package com.cu.sci.lambdaserver.user;
 
+import com.cu.sci.lambdaserver.auth.security.IAuthenticationFacade;
 import com.cu.sci.lambdaserver.user.service.IUserService;
+import com.cu.sci.lambdaserver.utils.enums.Role;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -13,6 +15,24 @@ import java.util.Collection;
 @RequiredArgsConstructor
 public class UserController {
     private final IUserService userService;
+    private final IAuthenticationFacade authenticationFacade;
+
+    @GetMapping("/me")
+    public Object getMyState() {
+        User user = authenticationFacade.getAuthenticatedUser();
+        if (user.getRoles().contains(Role.STUDENT)) {
+            // user to student
+
+
+            return user;
+        } else if (user.getRoles().contains(Role.PROFESSOR)) {
+
+        } else if (user.getRoles().contains(Role.ADMIN)) {
+
+        }
+
+        return user;
+    }
 
     @GetMapping("/{id}")
     public User getUserById(@PathVariable Long id) {
