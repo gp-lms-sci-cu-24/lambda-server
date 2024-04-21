40ea1b11c66d19fa5041ba3d4fb352cb021dd40b
HNOONa-0 <59091536+HNOONa-0@users.noreply.github.com>
2024-04-21 15:34:21 +0200

    add bulk insert,update students

--- src/main/java/com/cu/sci/lambdaserver/student/StudentRepository.java
+++ src/main/java/com/cu/sci/lambdaserver/student/StudentRepository.java
@@ -1,12 +1,17 @@
 package com.cu.sci.lambdaserver.student;
 
+import org.hibernate.annotations.CollectionIdJdbcTypeCode;
+import org.hibernate.sql.ast.tree.expression.Collation;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Collection;
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface StudentRepository extends JpaRepository<Student, Long>, PagingAndSortingRepository<Student, Long> {
     Optional<Student> findByCode(String code);
+    Collection<Student> getByCodeIn(List<String> studentsCode);
 }
--- src/main/java/com/cu/sci/lambdaserver/student/service/StudentService.java
+++ src/main/java/com/cu/sci/lambdaserver/student/service/StudentService.java
@@ -7,9 +7,7 @@ import com.cu.sci.lambdaserver.department.Department;
 import com.cu.sci.lambdaserver.department.DepartmentRepository;
 import com.cu.sci.lambdaserver.student.Student;
 import com.cu.sci.lambdaserver.student.StudentRepository;
-import com.cu.sci.lambdaserver.student.dto.CreateStudentRequestDto;
-import com.cu.sci.lambdaserver.student.dto.StudentDto;
-import com.cu.sci.lambdaserver.student.dto.UpdateStudentDto;
+import com.cu.sci.lambdaserver.student.dto.*;
 import com.cu.sci.lambdaserver.student.mapper.StudentMapper;
 import com.cu.sci.lambdaserver.utils.mapper.config.IMapper;
 import lombok.RequiredArgsConstructor;
@@ -22,7 +20,8 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
-import java.util.Optional;
+import java.util.*;
+import java.util.stream.Collectors;
 
 
 @Service
@@ -32,10 +31,16 @@ public class StudentService implements IStudentService {
     private final StudentRepository studentRepository;
     private final DepartmentRepository departmentRepository;
     private final IMapper<Student, CreateStudentRequestDto> createStudentRequestDtoMapper;
+    private final IMapper<Student, UpdateStudentDto> updateStudentDtoIMapper;
     private final IMapper<Student, StudentDto> studentDtoiMapper;
     private final PasswordEncoder passwordEncoder;
     private final ContactInfoService contactInfoService;
 
+    public void initStudent(Student student, Department department){
+        student.setDepartment(department );
+        student.setUsername(student.getCode() );
+        student.setPassword(passwordEncoder.encode(student.getPassword()));
+    }
     @Override
     public StudentDto creatStudent(CreateStudentRequestDto studentDto) throws ResponseStatusException {
         // check department
@@ -71,7 +76,7 @@ public class StudentService implements IStudentService {
         Page<Student> students = studentRepository.findAll(pageable);
         //check if list empty
         if (students.isEmpty()) {
-            throw new ResponseStatusException(HttpStatus.NO_CONTENT);
+            throw new ResponseStatusException(HttpStatus.NO_CONTENT, " did not find any students");
         }
         return students.map(studentDtoiMapper::mapTo);
     }
@@ -80,6 +85,8 @@ public class StudentService implements IStudentService {
     public StudentDto getStudent(String code) {
         Optional<Student> student = studentRepository.findByCode(code);
         if (student.isEmpty()) {
+            // this throws a null error when students table is empty
+//            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, " student not found with this code ");
         }
         return studentDtoiMapper.mapTo(student.get());
@@ -104,6 +111,52 @@ public class StudentService implements IStudentService {
         });
         return studentDtoiMapper.mapTo(student.get());
     }
+    @Override
+    public Collection<StudentDto> createStudents(CreateStudentsDto createStudentsDto) {
+        // insert without bulking for 10 k records: 1 minute and 39 seconds
+        // insert with bulking for 10 k records with batch size = 100: 18 seconds
+        // !!!!!!!!!!!!
+
+        // do not allow updates in departments
+        List<CreateStudentRequestDto> students = createStudentsDto.getStudents();
+        List<Student> studentList = new ArrayList<>(students.size() );
+        Map<String, Department> departmentMap = new HashMap<>();
+
+        for (int i = 0; i < students.size(); i++) {
+            Student student = createStudentRequestDtoMapper.mapFrom(students.get(i) );
+            Department department;
+            if(departmentMap.containsKey(students.get(i).getDepartmentCode() ) ){
+                department = departmentMap.get(students.get(i).getDepartmentCode() );
+            }
+            else{
+                System.out.println("calling db to get the department");
+                department = departmentRepository
+                        .findDepartmentByCodeIgnoreCase(students.get(i).getDepartmentCode())
+                        .orElseThrow(() -> new ResponseStatusException(HttpStatus.CONFLICT, "department not found with this code") );
+                departmentMap.put(students.get(i).getDepartmentCode(), department);
+            }
+            initStudent(student,department);
+            studentList.add(student);
+        }
+        return studentRepository.saveAll(studentList)
+                .stream().map(studentDtoiMapper::mapTo).collect(Collectors.toList());
+    }
+
+    @Override
+    public Collection<StudentDto> updateStudents(UpdateStudentsDto updateStudentsDto) {
+        Map<String, UpdateStudentDto> studentMap = updateStudentsDto.getStudents().stream()
+                .collect(Collectors.toMap(UpdateStudentDto::getCode, student -> student) );
+        Collection<Student> dbStudents = studentRepository
+                .getByCodeIn(
+                        updateStudentsDto.getStudents().stream().map(UpdateStudentDto::getCode).collect(Collectors.toList() )
+                );
+        for(Student student: dbStudents){
+            studentMap.get(student.getCode() ).setCode(null);
+            updateStudentDtoIMapper.update(studentMap.get(student.getCode() ), student);
+        }
+        return studentRepository.saveAll(dbStudents)
+                .stream().map(studentDtoiMapper::mapTo).collect(Collectors.toList());
+    }
 
     @Override
     public void deleteStudent(String code) {
@@ -118,5 +171,26 @@ public class StudentService implements IStudentService {
         //delete student
         studentRepository.delete(student.get());
     }
-
+//    public void bulkTest(){
+//        List<Student> studentList = new ArrayList<>();
+//        Department department = departmentRepository
+//                .findDepartmentByCodeIgnoreCase("GEN")
+//                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "department not found with this code") );
+//        for (int i = 0; i < 10000; i++) {
+//            Student student = new Student();
+//            student.setFirstName("FirstName" + i);
+//            student.setFatherName("FatherName" + i);
+//            student.setGrandfatherName("GrandfatherName" + i);
+//            student.setLastname("Lastname" + i);
+//            student.setCode("tode" + i);
+//            student.setUsername(student.getCode() );
+//            student.setPassword("student");
+//            student.setCreditHours(i * 3); // Just an example calculation for credit hours
+//            student.setAddress("Address" + i);
+//            student.setJoiningYear("2020"); // Example joining year
+//            student.setDepartment(department);
+//
+//            studentList.add(student);
+//        }
+//    }
 }
