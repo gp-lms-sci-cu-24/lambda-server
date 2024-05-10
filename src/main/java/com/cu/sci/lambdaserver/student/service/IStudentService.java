69a18b5d6f0992d16e5fe68c6eea951bb8dff65d
Abdallah Mohamed <abdallah.moh.153@gmail.com>
2024-05-10 15:31:41 +0300

    add authorize on student services

--- src/main/java/com/cu/sci/lambdaserver/student/service/IStudentService.java
+++ src/main/java/com/cu/sci/lambdaserver/student/service/IStudentService.java
@@ -2,20 +2,31 @@ package com.cu.sci.lambdaserver.student.service;
 
 import com.cu.sci.lambdaserver.student.dto.*;
 import org.springframework.data.domain.Page;
+import org.springframework.security.access.prepost.PreAuthorize;
 
 import java.util.Collection;
 
 public interface IStudentService {
+
+    @PreAuthorize("hasRole('ADMIN')")
     StudentDto creatStudent(CreateStudentRequestDto student);
 
+    @PreAuthorize("hasAnyRole('ADMIN','ACADEMIC_ADVISOR')")
     Page<StudentDto> getAllStudents(Integer pageNo, Integer pageSize);
 
+    @PreAuthorize("hasAnyRole('ADMIN','ACADEMIC_ADVISOR')")
     StudentDto getStudent(String code);
 
+    @PreAuthorize("hasRole('ADMIN')")
     StudentDto updateStudent(String code, UpdateStudentDto updateStudentDto);
+
+    @PreAuthorize("hasRole('ADMIN')")
     void deleteStudent(String code);
 
+    @PreAuthorize("hasRole('ADMIN')")
     Collection<StudentDto> createStudents(CreateStudentsDto createStudentsDto);
 
+    @PreAuthorize("hasRole('ADMIN')")
     Collection<StudentDto> updateStudents(UpdateStudentsDto updateStudentsDto);
+
 }
