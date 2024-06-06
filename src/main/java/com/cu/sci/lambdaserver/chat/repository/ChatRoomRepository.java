80d0b35f63ba2e2da35cf4f6fe21f3895f343935
Abdallah Mohamed <abdallah.moh.153@gmail.com>
2024-06-06 17:42:30 +0300

    add Create ChatRoom

--- src/main/java/com/cu/sci/lambdaserver/chat/repository/ChatRoomRepository.java
+++ src/main/java/com/cu/sci/lambdaserver/chat/repository/ChatRoomRepository.java
@@ -1,7 +1,13 @@
 package com.cu.sci.lambdaserver.chat.repository;
 
 import com.cu.sci.lambdaserver.chat.entites.ChatRoom;
+import com.cu.sci.lambdaserver.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ChatRoomRepository extends JpaRepository<ChatRoom, Long> {
+
+    Optional<ChatRoom> findByFirstUserAndSecondUser(User firstUser, User secondUser);
+
 }
---
+++ src/main/java/com/cu/sci/lambdaserver/chat/service/IChatRoomService.java
@@ -0,0 +1,12 @@
+package com.cu.sci.lambdaserver.chat.service;
+
+import com.cu.sci.lambdaserver.chat.entites.ChatRoom;
+
+public interface IChatRoomService {
+
+
+    void createChatRoom(String firstUserName, String secondUserName);
+
+
+
+}
