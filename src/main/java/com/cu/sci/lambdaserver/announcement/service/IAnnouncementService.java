47c69abe28e7ea34398468c881c721879ff77487
Abdallah Mohamed <abdallah.moh.153@gmail.com>
2024-06-08 23:48:12 +0300

    add crud Announcement

--- src/main/java/com/cu/sci/lambdaserver/announcement/service/IAnnouncementService.java
+++ src/main/java/com/cu/sci/lambdaserver/announcement/service/IAnnouncementService.java
@@ -3,11 +3,23 @@ package com.cu.sci.lambdaserver.announcement.service;
 import com.cu.sci.lambdaserver.announcement.dto.AnnouncementDto;
 import org.springframework.security.access.prepost.PreAuthorize;
 
+import java.util.Collection;
+
 public interface IAnnouncementService {
 
     @PreAuthorize("hasAnyRole('ADMIN','ACADEMIC_ADVISOR','PROFESSOR')")
     AnnouncementDto createAnnouncement(AnnouncementDto createAnnouncementDto);
 
+    @PreAuthorize("hasAnyRole('ADMIN','ACADEMIC_ADVISOR','PROFESSOR','STUDENT')")
+    Collection<AnnouncementDto> getAnnouncements();
 
+    @PreAuthorize("hasAnyRole('ADMIN','ACADEMIC_ADVISOR','PROFESSOR','STUDENT')")
+    AnnouncementDto getAnnouncement(Long announcementId);
+
+    @PreAuthorize("hasAnyRole('ADMIN','ACADEMIC_ADVISOR')")
+    AnnouncementDto updateAnnouncement(Long announcementId, AnnouncementDto updateAnnouncementDto);
+
+    @PreAuthorize("hasAnyRole('ADMIN','ACADEMIC_ADVISOR')")
+    void deleteAnnouncement(Long announcementId);
 
 }
