fb0696632460382cb80ef1e29d11a91bb20499de
Abdallah Mohamed <abdallah.moh.153@gmail.com>
2024-06-08 18:48:36 +0300

    add Announcement entity

---
+++ src/main/java/com/cu/sci/lambdaserver/announcement/Announcement.java
@@ -0,0 +1,32 @@
+package com.cu.sci.lambdaserver.announcement;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "announcements")
+public class Announcement {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "announcement_seq")
+    @SequenceGenerator(name = "announcement_seq", sequenceName = "announcement_seq", allocationSize = 10)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title ;
+
+    @Column(nullable = false)
+    private String description ;
+
+    private LocalDateTime timestamp;
+
+
+}
