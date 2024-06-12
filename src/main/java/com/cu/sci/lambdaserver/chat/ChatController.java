3317f9b8d3e2d7d8825ace66a3ae07ca184613ba
Abdallah Mohamed <abdallah.moh.153@gmail.com>
2024-06-12 15:19:22 +0300

    refactor socket

--- src/main/java/com/cu/sci/lambdaserver/chat/ChatController.java
+++ src/main/java/com/cu/sci/lambdaserver/chat/ChatController.java
@@ -2,14 +2,16 @@ package com.cu.sci.lambdaserver.chat;
 
 import com.cu.sci.lambdaserver.chat.dtos.MessageDto;
 import com.cu.sci.lambdaserver.chat.dtos.SendMessageDto;
-import com.cu.sci.lambdaserver.chat.entites.Message;
 import com.cu.sci.lambdaserver.chat.service.MessageService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 @RequiredArgsConstructor
 public class ChatController {
 
@@ -17,10 +19,11 @@ public class ChatController {
     private final SimpMessagingTemplate messagingTemplate;
 
 
-    @MessageMapping("/chat")
-    public void sendMassage(SendMessageDto sendMessage) {
+    @MessageMapping("/chat.sendMassage")
+    public void sendMassage(@Payload SendMessageDto sendMessage) {
+        System.out.println(sendMessage);
         MessageDto message = messageService.sendMessage(sendMessage);
-        messagingTemplate.convertAndSendToUser(message.getReceiver(), "/queue/messages", message);
+        messagingTemplate.convertAndSendToUser(message.getReceiver(), "/topic/messages", message);
     }
 
 }
--- src/main/java/com/cu/sci/lambdaserver/chat/config/WebSocketConfig.java
+++ src/main/java/com/cu/sci/lambdaserver/chat/config/WebSocketConfig.java
@@ -14,11 +14,12 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
         registry.enableSimpleBroker( "/topic");
+        registry.setApplicationDestinationPrefixes("/app");
         registry.setUserDestinationPrefix("/user");
     }
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/chat").setAllowedOrigins("*").withSockJS();
+        registry.addEndpoint("/ws").setAllowedOrigins("*");
     }
 }
