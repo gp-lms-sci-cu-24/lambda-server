cd5582fcf73d83fe57b486e365afd1531074cc48
Abdallah Mohamed <abdallah.moh.153@gmail.com>
2024-06-17 18:58:25 +0300

    add Pagination in AnnouncementRepository

--- src/main/java/com/cu/sci/lambdaserver/announcement/AnnouncementRepository.java
+++ src/main/java/com/cu/sci/lambdaserver/announcement/AnnouncementRepository.java
@@ -1,6 +1,7 @@
 package com.cu.sci.lambdaserver.announcement;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
-public interface AnnouncementRepository extends JpaRepository<Announcement, Long> {
+public interface AnnouncementRepository extends JpaRepository<Announcement, Long>, PagingAndSortingRepository<Announcement, Long>{
 }
