ac4df4f4f326e9b3bd8d7f67870aa56202e6c250
Abdallah Mohamed <abdallah.moh.153@gmail.com>
2024-06-26 21:12:13 +0300

    modify CreateContactInfoDto

--- src/main/java/com/cu/sci/lambdaserver/contactinfo/dto/CreateContactInfoDto.java
+++ src/main/java/com/cu/sci/lambdaserver/contactinfo/dto/CreateContactInfoDto.java
@@ -3,6 +3,7 @@ package com.cu.sci.lambdaserver.contactinfo.dto;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -15,16 +16,12 @@ import org.hibernate.validator.constraints.Length;
 @AllArgsConstructor
 public class CreateContactInfoDto {
 
-    @NotBlank(message = "phone cannot be null")
-    @Length(min = 11, max = 11, message = "phone must be 10 digits")
-    private String phone;
+    @NotBlank
+    @NotNull
+    private String value ;
 
-    @NotBlank(message = "telephone cannot be null")
-    @Length(min = 10, max = 10, message = "telephone must be 10 digits")
-    private String telephone;
-
-    @NotBlank(message = "email cannot be null")
-    @Email(message = "email must be valid")
-    private String email;
+    @NotBlank
+    @NotNull
+    private String contactType;
 
 }
